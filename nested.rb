2bc28728e74e70405fc87bbbb4bb9a446a3d971b
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-05 14:23:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,6 +55,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 return programmer_hash[:dennis_ritchie]=>:languages[0]
+		
 end
 
 def adding_matz
@@ -121,5 +123,4 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-return programmer_hash[:dennis_ritchie]=>:languages[0]
-end
+
